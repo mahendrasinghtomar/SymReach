9bdbd19166ac295f43978e43eaa49f63dc747307
mahendrasinghtomar <37445208+mahendrasinghtomar@users.noreply.github.com>
2018-07-19 08:32:23 +0530

    Update laubloomis.cpp

--- examples/Laubloomis/laubloomis.cpp
+++ examples/Laubloomis/laubloomis.cpp
@@ -11,13 +11,10 @@
 #include <array>
 #include <cmath>  
 
-using namespace GiNaC;
+using namespace GiNaC; 
 
-int PERFINDS;
-
-int ExampleMst = 3; // Laub-Loomis
 const int state_dim = 7;
-int input_dim = 1;
+const int input_dim = 0;
 template<typename Tx, typename Tu>
 Tx funcLj_system(Tx x, Tu u, Tx xx){
     xx[0] = 1.4*x[2] - 0.9*x[0];
@@ -31,6 +28,9 @@ Tx funcLj_system(Tx x, Tu u, Tx xx){
 }
 symbol x0("x[0]"), x1("x[1]"), x2("x[2]"), x3("x[3]"), x4("x[4]"), x5("x[5]"), x6("x[6]");
 std::vector<symbol> xs{x0, x1, x2, x3, x4, x5, x6};	// x_symbol
+
+std::vector<symbol> us;
+
 std::vector<ex> f{1.4*xs[2] - 0.9*xs[0],
 					2.5*xs[4] - 1.5*xs[1],
 					0.6*xs[6] - 0.8*xs[1]*xs[2],
@@ -43,73 +43,74 @@ std::vector<ex> f{1.4*xs[2] - 0.9*xs[0],
 int main() {
 	char ask = 'y';
 	double tau, finaltime;
-	int LinErrorMethod;
 	double l_max;
 	int morder, taylorTerms;
 	int ask_chk = 0;
 	while(ask == 'y')
 	{
+    double x[state_dim], r[state_dim], u[input_dim], ru[input_dim];
 
-    // std::vector<double> L_hat_storage;
-
-    double x[state_dim], r[state_dim], u[input_dim];
-
-double W;
-if(ExampleMst == 3)	//Laub-Loomis
-{
-	if(ask_chk==0)
+	double W;
 	{
-	tau = 0.1; // (0.1, 0.05), 0.01
-	finaltime = 20;
-	LinErrorMethod = 1;
-	morder = 50;	// zonotope order above which order reduction
-	l_max = 0.05;	//0.02
-	taylorTerms = 4;
-	PERFINDS = 1;	// 1(out of split only one tested), 2(perfind using both split sets)
-	W = 0.1;	// (0.01, 0.1) initial box width
-	}
-		x[0] = 1.2;
-	    x[1] = 1.05;
-		x[2] = 1.5;
-		x[3] = 2.4;
-		x[4] = 1;
-		x[5] = 0.1;
-		x[6] = 0.45;
-	    for(int i=0;i<state_dim;i++)
-			r[i] = W*0.5;
-	    u[0] = 0;
-}
-    // ReachableSet(state_dim, input_dim, tau, r, x, u, L_hat_storage, finaltime, LinErrorMethod, l_max, morder, taylorTerms);
-	    ReachableSet(state_dim, input_dim, tau, r, x, u, finaltime, LinErrorMethod, l_max, morder, taylorTerms);
-
-
-    std::cout << "Do you want to try another tau and finaltime?(y,n):";
-    std::cin >> ask;
-	if(ask!='y')
-		return 0;
-	char ask2 = 'n';
-	std::cout << "Repeat with previous parameters?(y,n):";
-	std::cin >> ask2;
-	if(ask2 != 'y')
-	{
-		std::cout << "enter tau = ";
-		std::cin >> tau;
-		std::cout << "enter finaltime =  ";
-		std::cin >> finaltime;
-		std::cout << "enter PERFINDS =  ";
-		std::cin >> PERFINDS;
-		std::cout << "enter l_max =  ";
-		std::cin >> l_max;
-		std::cout << "enter morder = ";
-		std::cin >> morder;
-		if(ExampleMst==3)
+		if(ask_chk==0)
 		{
-			std::cout << "enter W = ";
-			std::cin >> W;
+		tau = 0.1; 
+		finaltime = 20;
+		morder = 40;	// zonotope order above which order reduction
+		l_max = 0.05;	
+		taylorTerms = 4;
+		W = 0.01;	// initial box radius
 		}
+			x[0] = 1.2;
+			x[1] = 1.05;
+			x[2] = 1.5;
+			x[3] = 2.4;
+			x[4] = 1;
+			x[5] = 0.1;
+			x[6] = 0.45;
+			for(int i=0;i<state_dim;i++)
+				r[i] = W;
 	}
+		int no_of_steps = finaltime/tau;
+		no_of_steps = (no_of_steps != finaltime/tau) ? ((finaltime/tau)+1) : (finaltime/tau);
+		std::vector<std::vector<mstom::zonotope>> Zti(no_of_steps);
+		mstom::zonotope Z0;
 
-	ask_chk += 1;
+		TicToc reachtime; 
+		reachtime.tic();
+		ReachableSet(state_dim, input_dim, tau, r, x, ru, u, no_of_steps, 1, l_max, morder, taylorTerms, Zti, Z0);		
+		reachtime.toc();
+
+		// Storing projection of Zti along dimension 4. Dimension count starts from 1 (not 0).
+		std::cout << "Writing File" << std::endl;
+		wfile_time(Zti, 4, tau); //
+		std::cout << "Plotting" << std::endl;
+		plotfilled(Zti, 4, tau); // Plots the dimension 4 vs time
+
+		std::cout << "Do you want to try another tau and finaltime?(y,n):";
+		std::cin >> ask;
+		if(ask!='y')
+			return 0;
+		char ask2 = 'n';
+		std::cout << "Repeat with previous parameters?(y,n):";
+		std::cin >> ask2;
+		if(ask2 != 'y')
+		{
+			std::cout << "enter tau = ";
+			std::cin >> tau;
+			std::cout << "enter finaltime =  ";
+			std::cin >> finaltime;
+			std::cout << "enter morder = ";
+			std::cin >> morder;
+			std::cout << "enter l_max =  ";
+			std::cin >> l_max;
+			{
+				std::cout << "enter W = ";
+				std::cin >> W;
+			}
+		}
+
+		ask_chk += 1;
 	}
     return 0;
 }
